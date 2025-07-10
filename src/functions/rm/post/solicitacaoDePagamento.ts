ce9aa9ddd09530c23c3d80d222b4fa84c4fc3c83
Antonio Juevan <86933408+Juevan@users.noreply.github.com>
2025-07-10 12:21:28 -0300

    remove ttborcamento

--- src/functions/rm/post/solicitacaoDePagamento.ts
+++ src/functions/rm/post/solicitacaoDePagamento.ts
@@ -18,7 +18,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 
         const CODCOLIGADA = campos.codigoDaColigada === '1' ? campos.codigoDaColigada2 : campos.codigoDaColigada || '';
         const CODFILIAL = campos.codigoDaColigada === '1' ? campos.codigoDaFilial2 : campos.codigoDaFilial || '';
-        const CODCOLTBORCAMENTO = CODCOLIGADA === '30' ? CODCOLIGADA : '0';
+        //const CODCOLTBORCAMENTO = CODCOLIGADA === '30' ? CODCOLIGADA : '0';
 
         const codigos = {
             AD: '1.2.06',
@@ -241,7 +241,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
                 ['QUANTIDADE', item.qtdDoItem],
                 ['PRECOUNITARIO', item.valorDoItem],
                 ['VALORDESC', item.desconto],
-                ['CODCOLTBORCAMENTO', CODCOLTBORCAMENTO],
+                ['CODCOLTBORCAMENTO', '0'],
                 ['CODTBORCAMENTO', item.codigoDaNatureza]
             ].map(([tag, valor]) => XML.montaTag(tag, valor));
             return construirSecaoXML('TITMMOV', itemTags);
